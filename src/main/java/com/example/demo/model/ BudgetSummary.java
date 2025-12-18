c9cfd0abd80f1149747f54bb4ae88c8b0168e156
bala-amypo <bala@amypo.in>
2025-12-18 08:11:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ BudgetSummary.java
@@ -0,0 +1,21 @@
+@Entity
+public class BudgetSummary {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    private BudgetPlan budgetPlan;
+
+    private Double totalIncome;
+    private Double totalExpense;
+
+    private String status; // UNDER_LIMIT / OVER_LIMIT
+
+    @PrePersist
+    public void setTimestamp() {
+        generatedAt = LocalDateTime.now();
+    }
+
+    private LocalDateTime generatedAt;
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,14 @@
+@Entity
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role = "USER";
+}
