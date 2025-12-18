4bf1edd451e60973577c27caeeded6cb972f6fbb
bala-amypo <bala@amypo.in>
2025-12-18 15:58:40 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,17 +1,17 @@
-@OpenAPIDefinition(
- security = @SecurityRequirement(name = "bearerAuth")
-)
-@Configuration
-public class OpenApiConfig {
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-          .components(new Components()
-          .addSecuritySchemes("bearerAuth",
-          new SecurityScheme()
-            .type(SecurityScheme.Type.HTTP)
-            .scheme("bearer")
-            .bearerFormat("JWT")));
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests()
+            .requestMatchers("/auth/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
