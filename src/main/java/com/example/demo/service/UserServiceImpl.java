9f17395b788ea811c1a55fe65835bc21c043a8f0
bala-amypo <bala@amypo.in>
2025-12-18 08:26:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
