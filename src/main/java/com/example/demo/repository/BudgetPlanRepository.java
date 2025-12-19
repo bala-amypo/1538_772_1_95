8fea1e5c243dd05f73042eb573985cb0e4a44ddc
bala-amypo <bala@amypo.in>
2025-12-19 09:00:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/BudgetPlanRepository.java
+++ src/main/java/com/example/demo/repository/BudgetPlanRepository.java
@@ -1,7 +1,3 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.BudgetPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface BudgetPlanRepository extends JpaRepository<BudgetPlan, Long> {
+Optional<BudgetPlan> findByUserAndMonthAndYear(User user, Integer month, Integer year);
 }
--- src/main/java/com/example/demo/repository/TransactionLogRepository.java
+++ src/main/java/com/example/demo/repository/TransactionLogRepository.java
@@ -1,7 +1,4 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.TransactionLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
+List<TransactionLog> findByUser(User user);
+List<TransactionLog> findByUserAndTransactionDateBetween(User user, LocalDate start, LocalDate end);
 }
