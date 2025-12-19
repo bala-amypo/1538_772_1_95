0ec8d08c8ab9ea4abd7ea51927f9f5b77c9e882f
bala-amypo <bala@amypo.in>
2025-12-19 04:58:25 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/CategoryServiceImpl.java
@@ -1,25 +1,28 @@
-package com.example.budget.service.impl;
+package com.example.demo.service.impl;
 
-import com.example.budget.model.Category;
-import com.example.budget.repository.CategoryRepository;
-import com.example.budget.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.service.CategoryService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
-    public Category addCategory(Category category) {
+    public Category save(Category category) {
         return categoryRepository.save(category);
     }
 
     @Override
-    public List<Category> getAllCategories() {
+    public List<Category> findAll() {
         return categoryRepository.findAll();
     }
 }
