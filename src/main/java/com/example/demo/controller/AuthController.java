3ffa55a1b24c05fee7a70f95d7e1789e9dfcceaf
bala-amypo <bala@amypo.in>
2025-12-19 04:02:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -15,20 +15,3 @@ public class AuthController {
         return new AuthResponse(token);
     }
 }
-CategoryController
-@RestController
-@RequestMapping("/categories")
-public class CategoryController {
-
-    @PostMapping
-    public Category create(@RequestBody Category c) {
-        if(repo.existsByName(c.getName()))
-            throw new RuntimeException("Duplicate category");
-        return repo.save(c);
-    }
-
-    @GetMapping
-    public List<Category> list() {
-        return repo.findAll();
-    }
-}
