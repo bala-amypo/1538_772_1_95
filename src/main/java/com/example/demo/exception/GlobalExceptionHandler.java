4fb516afcfb1690df5bec9167a08f7db99d7beac
bala-amypo <bala@amypo.in>
2025-12-21 12:16:51 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,28 +1,18 @@
 package com.example.demo.exception;
 
-
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
 
-@ExceptionHandler(BadRequestException.class)
-public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
-return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-}
-
-
-@ExceptionHandler(ConflictException.class)
-public ResponseEntity<String> handleConflict(ConflictException ex) {
-return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
-}
-
-
-@ExceptionHandler(Exception.class)
-public ResponseEntity<String> handleGeneral(Exception ex) {
-return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-}
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflict(ConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
 }
