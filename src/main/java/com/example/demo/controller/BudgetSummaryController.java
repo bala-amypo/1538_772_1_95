3a388fd7f3e14a9ad9f9f12f5f7814977b464608
bala-amypo <bala@amypo.in>
2025-12-23 04:43:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/BudgetSummaryController.java
+++ src/main/java/com/example/demo/controller/BudgetSummaryController.java
@@ -14,8 +14,15 @@ public class BudgetSummaryController {
         this.service = service;
     }
 
+    // GET summary by userId
     @GetMapping("/{userId}")
     public BudgetSummary getSummary(@PathVariable Long userId) {
         return service.getByUserId(userId);
+    }
+
+    // POST create budget summary
+    @PostMapping
+    public BudgetSummary createSummary(@RequestBody BudgetSummary budgetSummary) {
+        return service.save(budgetSummary);
     }
 }
